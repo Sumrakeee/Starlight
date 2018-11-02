3a2ed538b6e00298f4512100f535b0f542acb41e
Sumrak <44667695+Sumrakeee@users.noreply.github.com>
2018-11-02 14:10:05 +0300

    Update StarlightSelenium.py

--- StarlightSelenium.py
+++ StarlightSelenium.py
@@ -8,6 +8,20 @@ from bs4 import BeautifulSoup
 import os
 
 browser = False
+uid = ''
+path = 'C:\\Users\\SerovaSG\\Desktop\\Qt\\Test\\'
+
+def get_total_pages(url):
+	html = requests.get(url).text
+	soup = BeautifulSoup(html, 'lxml')
+	pages = soup.find('div', class_='pagination-pages').find_all('a', class_='pagination-page')[-1].get('href')
+	total_pages = pages.split('=')[1].split('&')[0]
+
+	return int(total_pages)
+
+def get_id(url):
+	global uid
+	uid = url.split('_')[-1]
 
 def launch_browser():
 	global driver
@@ -26,43 +40,86 @@ def open_page(url):
 	global driver
 
 	driver.get(url)
-	print(url)
-	driver.find_element_by_class_name('item-map-control').click()
-	print('map closed -OK')
-
+	print('page has been opened -OK')
+	try:
+		driver.find_element_by_class_name('item-map-control').click()
+		print('map closed -OK')
+	except:
+		print('unable to close map -ERROR')
+		pass
+	
 def take_screenshot():
 	global driver
 
 	driver.save_screenshot('img.png')
 	print('screenshot saved -OK')
 
-def create_document(filename):
+def create_document():
+	global uid
+	global path
 	doc = Document()
 	doc.add_picture('img.png', width=Inches(4))
-	doc.save(filename + '.docx')
+	doc.save(path + uid + '.docx')
+	print('document saved -OK')
 
-def browser_logic_hub(url, filename):
+def browser_logic_hub(url):
 	global browser
-	if browser == False:
-		launch_browser()
-		open_page(url)
-		print('page has been opened -OK')
-		take_screenshot()
-		create_document(filename)
-		browser = True
-	else:
-		open_page(url)
-		print('page has been opened -OK')
-		take_screenshot()
-		create_document(filename)
+
+	html = requests.get(url).text
+	soup = BeautifulSoup(html, 'lxml')
+	ads = soup.find('div', class_='js-catalog-list').find_all('div', class_='item_list')
+
+	for ad in ads:
+		url = 'https://www.avito.ru' + ad.find('div', class_='description-title').find('h3').find('a').get('href')
+		print(url)            
+		get_id(url)
+
+		if browser == False:
+			launch_browser()
+			open_page(url)
+			take_screenshot()
+			create_document()
+			browser = True
+		else:
+			open_page(url)
+			take_screenshot()
+			create_document()
+
+		#time.sleep(2)
 
 def main():
-	urls = ['https://www.avito.ru/stavropol/kvartiry/2-k_kvartira_65_m_59_et._1676844596']
-			#,'https://www.avito.ru/stavropol/zemelnye_uchastki/uchastok_16_sot._snt_dnp_1551738138']
+	print('Starlight v1.2S.02112018',
+		'\nРазработчики: Чернышев Егор Владимирович',
+		'\n\t      Чернышева Татьяна Анатольевна',
+		'\n\nФункционал ограничен',
+		'\n===========================================')
 
-	for url in urls:
-		uid = url.split('_')[-1]
-		browser_logic_hub(url, uid)
+	p_url = input('\nВВЕДИТЕ ССЫЛКУ: ')
+	startup_page = int(input('\nВВЕДИТЕ НОМЕР СТРАНИЦЫ, С КОТОРОЙ ХОТИТЕ НАЧАТЬ: '))
+
+	base_part = p_url.split('?')[0]
+	query_part = '?&s=104&view=list'
+	page_part = '&p='
+
+	total_pages = get_total_pages(p_url)
+
+	print('\nПРОГРАММА НАЧАЛА ВЫПОЛНЕНИЕ РАБОТЫ')
+
+	#try:
+	for i in range(startup_page, total_pages + 1):
+		url_gen = base_part + query_part + page_part + str(i)
+		print('\nСГЕНЕРИРОВАНА ССЫЛКА: '+ url_gen +'\n')
+		browser_logic_hub(url_gen)
+
+		print('\nСТРАНИЦА ОБРАБОТАНА')
+	#except:
+	#	url_gen = base_part + query_part + page_part + str(1)
+	#	print('\nСГЕНЕРИРОВАНА ССЫЛКА: '+ url_gen +'\n')
+	#	browser_logic_hub(url_gen)
+
+	#	print('\nСТРАНИЦА ОБРАБОТАНА')
+
+	print('\nПРОГРАММА ЗАВЕРШИЛА РАБОТУ')
 
 if __name__ == '__main__':
 	main()
