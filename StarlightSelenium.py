07d4199b4c7ffb73c46ac4d32d72579fb968e4d3
Sumrak <44667695+Sumrakeee@users.noreply.github.com>
2018-11-02 11:58:46 +0300

    Update StarlightSelenium.py

--- StarlightSelenium.py
+++ StarlightSelenium.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
 from selenium.webdriver import Chrome
+from docx import Document
+from docx.shared import Inches
 
 browser = False
 
-class BrowserStuff():
+class SelBrowser():
 	def launch_browser():
 		global driver
 		print('browser initialization')
@@ -13,7 +15,7 @@ class BrowserStuff():
 		chromeOptions.add_argument('--ignore-certificate-errors')
 		chromeOptions.add_argument("--test-type")
 		chromeOptions.add_argument("--window-size=1280x3000")
-		driver = webdriver.Chrome(chrome_options=chromeOptions)
+		driver = webdriver.Chrome(options=chromeOptions)
 
 		print('browser launched')
 		return driver
@@ -23,27 +25,29 @@ class BrowserStuff():
 
 		driver.get(url)
 		print(url)
+		driver.find_element_by_class_name('item-map-control').click()
+		print('map closed')
 		driver.save_screenshot(filename)
 		print('screenshot saved')
 
-def browser_logic(url, filename):
-	global browser
-	if browser == False:
-		BrowserStuff.launch_browser()
-		BrowserStuff.open_page(url, filename)
-		print('page has been opened')
-		browser = True
-	else:
-		BrowserStuff.open_page(url, filename)
-		print('page has been opened')
+	def browser_logic(url, filename):
+		global browser
+		if browser == False:
+			SelBrowser.launch_browser()
+			SelBrowser.open_page(url, filename)
+			print('page has been opened')
+			browser = True
+		else:
+			SelBrowser.open_page(url, filename)
+			print('page has been opened')
 
 def main():
 	filename = 0
-	urls = ['https://www.avito.ru/stavropol/kvartiry/2-k_kvartira_65_m_59_et._1676844596',
-			'https://www.avito.ru/stavropol/zemelnye_uchastki/uchastok_16_sot._snt_dnp_1551738138']
-			
+	urls = ['https://www.avito.ru/stavropol/kvartiry/2-k_kvartira_65_m_59_et._1676844596']
+			#,'https://www.avito.ru/stavropol/zemelnye_uchastki/uchastok_16_sot._snt_dnp_1551738138']
+
 	for url in urls:
-		browser_logic(url, str(filename)+'.png')
+		SelBrowser.browser_logic(url, str(filename)+'.png')
 		filename += 1
 
 if __name__ == '__main__':
